e712df6a10e86815a8d44c637627e5d12016dbf8
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2021-02-16 10:39:29 -0500

    Optimisated the installation wizard

--- installation_wizard.py
+++ installation_wizard.py
@@ -3,35 +3,33 @@ import os
 import subprocess
 from time import time
 
-directory = '\\minecraft_datapacks_generator'
+# Save the name of the directory and the path to it into variables
+directory = '\\minecraft_datapacks_generator2'
+folder = f'C:\\Users\\{os.getlogin()}\\AppData\\Local\\Programs\\Python\\Python39\\Lib' if os.name == 'nt' else '/usr/lib/python3.9'
 
-if os.name == "nt":
-    folder = f'C:\\Users\\{os.getlogin()}\\AppData\\Local\\Programs\\Python\\Python39\\Lib'
-    
-    if os.path.exists(f'{folder}{directory}') and os.path.isdir(f'{folder}{directory}'):
-        print('Found at:', f'{folder}{directory}, replace this version with the latest release.')
-    else:
-        if input('Do you want to scan your computer to look for the folder? [yes/no]: ')[0].lower() == 'y':
-            found = False
-            directory_list = str(folder + '\\site-packages').split('\\')
-            tree = ['\\'.join(directory_list[:i + 1]) for i in range(len(directory_list))][::-1]
+if os.path.exists(f'{folder}{directory}') and os.path.isdir(f'{folder}{directory}'):
+    print(f'Found at: {folder}{directory}, replace this version with the latest release.')
+else:
+    if input('Do you want to scan your computer to look for the folder? [yes/no]: ')[0].lower() == 'y':
+        found = False
+        directory_list = str(folder + '\\site-packages').split('\\')
+        tree = ['\\'.join(directory_list[:i + 1]) for i in range(len(directory_list))][::-1]
+
+        try:
+            time0 = time()
             HDDs = tree + [e + ':\\' for e in 'CDEFGHIJK' if os.path.exists(e + ':\\')]
 
             for HDD in HDDs:
                 if not found:
-                    time0 = time()
+                    print('walking in', HDD)
                     for path, dirs, files in os.walk(HDD):
                         if folder + directory in path:
                             found = True
                             print(f'Found at: {path} in {time() - time0:0.1} seconds.')
                             break
-        print(f'Paste the "minecraft_datapack_generator" folder in here: {folder}{directory}.')
-else:
-    folder = f'/usr/lib/python3.9'
-    if os.path.exists(f'{folder}{directory}'):
-        print('Found at:', f'{folder}{directory}, replace this version with the latest release.')
-    else:
-        print(f'Paste the "minecraft_datapack_generator" folder in here: {folder}{directory}.')
-
+        except OSError as e:
+            print(e)
+        else:
+            print(f'Paste the "minecraft_datapack_generator" folder in here: {folder}{directory}.')
 
 input('Press "enter" to close this window.')
