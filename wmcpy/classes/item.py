e8d562ced5ac530e4d0b301aeecfa6f13409db92
theskyblockman <63855457+theskyblockman@users.noreply.github.com>
2021-02-23 17:36:05 +0100

    fixing fixage lol

--- wmcpy/classes/item.py
+++ wmcpy/classes/item.py
@@ -3,11 +3,11 @@ class Item():
         self.item_name = item_name.replace('minecraft:', '', 1)
         self.tag = tag
         self.count = count
-    
+
     @property
     def data_form(self) -> dict:
         return {'id': 'minecraft:' + self.item_name, 'Count': self.count, 'tag': self.tag}
-    
+
     @property
     def give_form(self) -> str:
         return self.item_name + str(self.tag) + ' ' + self.count
